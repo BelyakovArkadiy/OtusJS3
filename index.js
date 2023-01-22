536217ef2d34b29b5610494612d479e276291284
belakov <belyakov@kolesa.kz>
2023-01-22 19:15:26 +0600

    hw5

--- index.js
+++ index.js
@@ -45,10 +45,87 @@
 
 // npm test
 
-import { strict as assert } from 'node:assert';
+// import { strict as assert } from 'node:assert';
 
-assert.equal(sum(2, 2), 4)
+// assert.equal(sum(2, 2), 4)
 
-function sum(a,b) {
-    return a+b
-}
+// function sum(a,b) {
+//     return a+b
+// }
+
+// function getPersonInfo( name, surname, age){
+//    return `Name:${name}. Surname:${surname}. Age:${age}`
+// }
+// const personInfo = getPersonInfo('Kurt', 'Ten', '40')
+// console.log(personInfo);
+// console.dir(personInfo); 
+
+// const person2 = {
+//     namePerson: null, 
+//     surnamePerson: null,
+//     agePerson: null
+// }
+// console.log(person2);
+
+// person2.namePerson = 'Roma'
+// person2.surnamePerson = 'Dota'
+// person2.agePerson = 12
+
+// console.log(person2);
+
+// function getPersonInfo(person2){
+//        return `Name:${person2.namePerson}. Surname:${person2.surnamePerson}. Age:${person2.agePerson}`
+//     }
+
+//     console.log(getPersonInfo(person2));
+
+//     const person2 = {
+//         namePerson: null, 
+//         surnamePerson: null,
+//         agePerson: null,
+//         showInfo() {
+//     console.log( `Name:${this.namePerson}. Surname:${this.surnamePerson}. Age:${this.agePerson}`)
+//         }
+//     }
+
+//     person2.agePerson     = 30
+//     person2.namePerson    = 'Tom'
+//     person2.surnamePerson = 'JJ'
+
+//     person2.showInfo();
+// // для объекта
+//     for (const key in person2){
+//         console.log(`key: `, key, `value: `, person2[key])
+//     }
+
+//     const array = [100, 200, 300]
+//     for(let i =0; i < array.length; i++){
+//         array[i] = array[i] * 2
+//     }
+//     console.log(array);
+// // для массива
+//     for (const value of array){
+//         console.log(`значение: `, value)
+//     }
+
+
+
+    const scores = {
+       Anna : 10,
+       Dana : 32,
+       Giza : 13,
+       Nina : 21
+    }
+
+    function getScores(scores)
+    {
+         let scoreUsers = 0
+        for(const key in scores)
+        {
+        scoreUsers += scores[key];
+        }
+         return console.log(scoreUsers);
+       }
+
+        getScores(scores);
+
